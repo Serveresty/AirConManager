b259693bbab52010d46785feb89079370c7cd4f7
Serveresty <cs.go.12228@gmail.com>
2024-10-23 20:58:25 +0300

    cool input window doxygen comms

--- includes/coolinputwindow.h
+++ includes/coolinputwindow.h
@@ -28,31 +28,83 @@ class CoolInput : public QDialog
     Q_OBJECT
 
 public:
+    /**
+     * @brief Конструктор класса CoolInput.
+     * 
+     * Создаёт экземпляр окна ввода данных с элементами управления
+     * для ввода температуры, влажности и давления.
+     * 
+     * @param parent Указатель на родительский объект.
+     */
     explicit CoolInput(QWidget *parent = nullptr);
+
+    /**
+     * @brief Деструктор класса CoolInput.
+     * 
+     * Освобождает ресурсы, используемые экземпляром класса.
+     */
     ~CoolInput();
+
+    /**
+     * @brief Устанавливает минимальное и максимальное значения для поля ввода температуры.
+     * 
+     * @param minT Минимальное значение температуры.
+     * @param maxT Максимальное значение температуры.
+     */
     void setMinMaxTempUnit(double minT, double maxT);
+
+    /**
+     * @brief Устанавливает минимальное и максимальное значения для поля ввода влажности.
+     * 
+     * @param minH Минимальное значение влажности.
+     * @param maxH Максимальное значение влажности.
+     */
     void setMinMaxHumUnit(double minH, double maxH);
+
+    /**
+     * @brief Устанавливает минимальное и максимальное значения для поля ввода давления.
+     * 
+     * @param minP Минимальное значение давления.
+     * @param maxP Максимальное значение давления.
+     */
     void setMinMaxPresUnit(double minP, double maxP);
+
+    /**
+     * @brief Устанавливает текущие значения для полей ввода.
+     * 
+     * @param tmp Значение температуры.
+     * @param hum Значение влажности.
+     * @param pres Значение давления.
+     */
     void setCurrentValues(double tmp, double hum, double pres);
+
 signals:
+    /**
+     * @brief Сигнал, отправляемый при подтверждении ввода данных.
+     * 
+     * @param tData Значение температуры.
+     * @param hData Значение влажности.
+     * @param pData Значение давления.
+     */
     void sendInputData(double tData, double hData, double pData);
+
 private:
-    QVBoxLayout *mainLayout;
+    QVBoxLayout *mainLayout; ///< Главная компоновка для размещения элементов управления.
 
-    QHBoxLayout *tempLayout;
-    QLabel *tempLabel;
-    QDoubleSpinBox *tempDSpinBox;
+    QHBoxLayout *tempLayout; ///< Компоновка для размещения элементов управления температурой.
+    QLabel *tempLabel; ///< Метка для отображения текста "Температура".
+    QDoubleSpinBox *tempDSpinBox; ///< Поле для ввода температуры.
 
-    QHBoxLayout *humLayout;
-    QLabel *humLabel;
-    QDoubleSpinBox *humDSpinBox;
+    QHBoxLayout *humLayout; ///< Компоновка для размещения элементов управления влажностью.
+    QLabel *humLabel; ///< Метка для отображения текста "Влажность".
+    QDoubleSpinBox *humDSpinBox; ///< Поле для ввода влажности.
 
-    QHBoxLayout *presLayout;
-    QLabel *presLabel;
-    QDoubleSpinBox *presDSpinBox;
+    QHBoxLayout *presLayout; ///< Компоновка для размещения элементов управления давлением.
+    QLabel *presLabel; ///< Метка для отображения текста "Давление".
+    QDoubleSpinBox *presDSpinBox; ///< Поле для ввода давления.
 
-    QHBoxLayout *confirmLayout;
-    QPushButton *confirmButton;
+    QHBoxLayout *confirmLayout; ///< Компоновка для размещения кнопки подтверждения.
+    QPushButton *confirmButton; ///< Кнопка для подтверждения ввода данных.
 };
 
 #endif
--- src/coolinputwindow.cpp
+++ src/coolinputwindow.cpp
@@ -8,75 +8,113 @@
  * который отвечает за создание и управление интерфейсом ввода данных.
  */
 
+/**
+ * @brief Конструктор класса CoolInput.
+ * 
+ * Создаёт экземпляр окна ввода данных с элементами управления
+ * для ввода температуры, влажности и давления.
+ * 
+ * @param parent Указатель на родительский объект.
+ */
 CoolInput::CoolInput(QWidget *parent)
     : QDialog(parent)
 {
     this->setFixedSize(300,300);
 
-    mainLayout = new QVBoxLayout;
-    tempLayout = new QHBoxLayout;
-    humLayout = new QHBoxLayout;
-    presLayout = new QHBoxLayout;
-    confirmLayout = new QHBoxLayout;
+    mainLayout = new QVBoxLayout; ///< Главная компоновка для размещения элементов управления.
+    tempLayout = new QHBoxLayout; ///< Компоновка для размещения элементов управления температурой.
+    humLayout = new QHBoxLayout; ///< Компоновка для размещения элементов управления влажностью.
+    presLayout = new QHBoxLayout; ///< Компоновка для размещения элементов управления давлением.
+    confirmLayout = new QHBoxLayout; ///< Компоновка для размещения кнопки подтверждения.
 
-    tempLabel = new QLabel("Температура: ", this);
-    humLabel = new QLabel("Влажность: ", this);
-    presLabel = new QLabel("Давление: ", this);
+    tempLabel = new QLabel("Температура: ", this); ///< Метка для отображения текста "Температура".
+    humLabel = new QLabel("Влажность: ", this); ///< Метка для отображения текста "Влажность".
+    presLabel = new QLabel("Давление: ", this); ///< Метка для отображения текста "Давление".
 
-    tempDSpinBox = new QDoubleSpinBox;
-    humDSpinBox = new QDoubleSpinBox;
-    presDSpinBox = new QDoubleSpinBox;
+    tempDSpinBox = new QDoubleSpinBox; ///< Поле для ввода температуры.
+    humDSpinBox = new QDoubleSpinBox; ///< Поле для ввода влажности.
+    presDSpinBox = new QDoubleSpinBox; ///< Поле для ввода давления.
 
-    confirmButton = new QPushButton("Применить", this);
+    confirmButton = new QPushButton("Применить", this); ///< Кнопка для подтверждения ввода данных.
 
-    tempLayout->addWidget(tempLabel);
-    tempLayout->addWidget(tempDSpinBox);
+    tempLayout->addWidget(tempLabel); ///< Добавление метки температуры в компоновку.
+    tempLayout->addWidget(tempDSpinBox); ///< Добавление поля ввода температуры в компоновку.
 
-    humLayout->addWidget(humLabel);
-    humLayout->addWidget(humDSpinBox);
+    humLayout->addWidget(humLabel); ///< Добавление метки влажности в компоновку.
+    humLayout->addWidget(humDSpinBox); ///< Добавление поля ввода влажности в компоновку.
 
-    presLayout->addWidget(presLabel);
-    presLayout->addWidget(presDSpinBox);
+    presLayout->addWidget(presLabel); ///< Добавление метки давления в компоновку.
+    presLayout->addWidget(presDSpinBox); ///< Добавление поля ввода давления в компоновку.
 
-    confirmLayout->addWidget(confirmButton);
+    confirmLayout->addWidget(confirmButton); ///< Добавление кнопки подтверждения в компоновку.
 
-    mainLayout->addLayout(tempLayout);
-    mainLayout->addLayout(humLayout);
-    mainLayout->addLayout(presLayout);
-    mainLayout->addLayout(confirmLayout);
+    mainLayout->addLayout(tempLayout); ///< Добавление компоновки температуры в главную компоновку.
+    mainLayout->addLayout(humLayout); ///< Добавление компоновки влажности в главную компоновку.
+    mainLayout->addLayout(presLayout); ///< Добавление компоновки давления в главную компоновку.
+    mainLayout->addLayout(confirmLayout); ///< Добавление компоновки подтверждения в главную компоновку.
 
-    this->setLayout(mainLayout);
+    this->setLayout(mainLayout); ///< Установка главной компоновки как основной компоновки окна.
 
     connect(confirmButton, &QPushButton::clicked, this, [=](){
-        double tData = tempDSpinBox->value();
-        double hData = humDSpinBox->value();
-        double pData = presDSpinBox->value();
-        emit sendInputData(tData, hData, pData);
+        double tData = tempDSpinBox->value(); ///< Получение значения температуры.
+        double hData = humDSpinBox->value(); ///< Получение значения влажности.
+        double pData = presDSpinBox->value(); ///< Получение значения давления.
+        emit sendInputData(tData, hData, pData); ///< Отправка введённых данных через сигнал.
     });
 }
 
+/**
+ * @brief Устанавливает минимальное и максимальное значения для поля ввода температуры.
+ * 
+ * @param minT Минимальное значение температуры.
+ * @param maxT Максимальное значение температуры.
+ */
 void CoolInput::setMinMaxTempUnit(double minT, double maxT) {
     tempDSpinBox->setMinimum(minT);
     tempDSpinBox->setMaximum(maxT);
 }
 
+/**
+ * @brief Устанавливает минимальное и максимальное значения для поля ввода влажности.
+ * 
+ * @param minH Минимальное значение влажности.
+ * @param maxH Максимальное значение влажности.
+ */
 void CoolInput::setMinMaxHumUnit(double minH, double maxH) {
     humDSpinBox->setMinimum(minH);
     humDSpinBox->setMaximum(maxH);
     humDSpinBox->setDecimals(0);
 }
 
+/**
+ * @brief Устанавливает минимальное и максимальное значения для поля ввода давления.
+ * 
+ * @param minP Минимальное значение давления.
+ * @param maxP Максимальное значение давления.
+ */
 void CoolInput::setMinMaxPresUnit(double minP, double maxP) {
     presDSpinBox->setMinimum(minP);
     presDSpinBox->setMaximum(maxP);
 }
 
+/**
+ * @brief Устанавливает текущие значения для полей ввода.
+ * 
+ * @param tmp Значение температуры.
+ * @param hum Значение влажности.
+ * @param pres Значение давления.
+ */
 void CoolInput::setCurrentValues(double tmp, double hum, double pres) {
     tempDSpinBox->setValue(tmp);
     humDSpinBox->setValue(hum);
     presDSpinBox->setValue(pres);
 }
 
+/**
+ * @brief Деструктор класса CoolInput.
+ * 
+ * Освобождает ресурсы, используемые экземпляром класса.
+ */
 CoolInput::~CoolInput()
 {
 }
